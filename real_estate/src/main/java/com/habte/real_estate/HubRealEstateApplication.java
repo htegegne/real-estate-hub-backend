ab5cf1ca05b9567f901f985861b6e09c2e2d8987
HabTeg21 <81588184+HabTeg21@users.noreply.github.com>
2023-12-31 18:02:30 -0600

    project setup
    Created using Spring Boot initializer

---
+++ real_estate/src/main/java/com/habte/real_estate/HubRealEstateApplication.java
@@ -0,0 +1,14 @@
+package com.habte.real_estate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubRealEstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubRealEstateApplication.class, args);
+		System.out.println("Welcome");
+	}
+
+}
---
+++ real_estate/src/test/java/com/habte/real_estate/HubRealEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habte.real_estate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubRealEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
